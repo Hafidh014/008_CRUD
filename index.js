e7750a225ee306b7c85d7e4912ca0aacce00e563
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-07-15 14:21:44 +0700

    Pengaturan Awal Server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mahasiswa',
+    password: '12345678',
+    port: 5432,
+});
